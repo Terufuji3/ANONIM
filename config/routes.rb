576404f2a5722721b9c6a6bd12b21a59ee6d3683
Terufuji3 <terufuji.med@gmail.com>
2022-02-13 02:31:23 +0900

    Add sign_out function

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations',
+                                    sessions: 'users/sessions' }
   root 'static_pages#top'
   get 'terms', to: 'static_pages#terms'
 
