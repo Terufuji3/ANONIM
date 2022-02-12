1edc68e34ee9251f8ff541298f95e1afb7ac6b06
Terufuji3 <terufuji.med@gmail.com>
2022-02-13 00:38:00 +0900

    Add name column to users table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+    #アカウント登録時
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    #登録情報更新時
+    before_action :configure_account_update_parameters, if: :devise_controller?
+
+    protected
+        #アカウント登録時
+        def configure_permitted_parameters
+            devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+        end
+
+        #登録情報更新時
+        def configure_account_update_parameters
+            devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+        end
 end
